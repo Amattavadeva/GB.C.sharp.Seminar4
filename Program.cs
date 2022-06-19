a976201fa8c3fb427a782b16e07398e9a07f56dc
Alex QAsper <semidog@gmail.com>
2022-06-19 14:29:03 +0300

    rewrite 25 tast

--- Program.cs
+++ Program.cs
@@ -10,14 +10,13 @@ int B = int.Parse(Console.ReadLine());
 void Fill(int num, int degree)
 {
     int num_n = 1;
-    for (int i = 0; i < degree+1; i++)
+    for (int i = 0; i < degree; i++)
     {
         num_n *= num;
     }
     Console.WriteLine("Ответ: " + num_n);
 }
 Fill(A, B);
-
 
 
 /* Задача 27: Напишите программу, которая принимает на
